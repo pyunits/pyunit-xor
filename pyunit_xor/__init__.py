34aa0e023a70d5a54a21d13b07178b0782bc18b2
zhangwei <jtyoui@qq.com>
2025-04-11 16:02:24 +0800

    增加XOR

--- pyunit_xor/__init__.py
+++ pyunit_xor/__init__.py
@@ -1,1 +1,8 @@
 from .password import XORException, SimpleXOR
+
+__version__ = '2025.4.11'
+__author__ = '张伟'
+__description__ = '一个简单的异或生成Token'
+__email__ = 'jtyoui@qq.com'
+__names__ = 'pyUnit_xor'
+__url__ = 'https://github.com/pyunits/pyunit-xor'
